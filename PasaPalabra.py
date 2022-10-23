bdca788db3ec98da293bf98825d010dd1ac8fa1a
Matias <smatiias@gmail.com>
2022-10-22 22:29:33 -0300

    tercer commit

---
+++ PasaPalabra.py
@@ -0,0 +1,8 @@
+import string
+from typing import List
+
+print(string.ascii_uppercase)
+abecedario = [string.ascii_uppercase]
+print(f"{abecedario}")
+entrada = input("Ingrese \"P\" para obtener una letra: ").upper()
+# if entrada == "P":
